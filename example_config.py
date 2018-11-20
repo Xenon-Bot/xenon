f1aee25bd420ae446c8b2303e35c8ecc5fcdaa3c
Merlintor <merlinfuchs2001@gmail.com>
2018-11-20 20:00:14 +0100

    Added example config

---
+++ example_config.py
@@ -0,0 +1,16 @@
+# Rename this to "config.py"
+
+token = "your-bot-token"
+shard_count = 1  # ~ guilds // 1000
+shard_ids = None  # ~ Optional
+
+prefix = "x!"
+
+extensions = [
+    "cogs.errors",
+    "cogs.help",
+    "cogs.admin",
+    "cogs.backups",
+    "cogs.templates",
+    "cogs.pro"
+]
