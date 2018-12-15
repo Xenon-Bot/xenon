b2f0d357cfe3bd2e214654c4502c488aa477965f
Merlintor <merlinfuchs2001@gmail.com>
2018-12-15 17:27:12 +0100

    Implemented ban and message sync

--- cogs/pro.py
+++ cogs/pro.py
@@ -1,7 +1,7 @@
 from discord.ext import commands as cmd
 from discord_backups import copy_guild
 
-from utils import checks
+from utils import checks, helpers
 from cogs import backups
 
 
@@ -29,7 +29,7 @@ class Pro:
     @cmd.cooldown(1, 5 * 60, cmd.BucketType.guild)
     async def copy(self, ctx, guild_id: int, chatlog: int = backups.max_chatlog):
         """
-        Copy all channels and roles from one guild to another (this)
+        Copy all channels and roles from another guild to this guild
 
 
         guild_id ::     The id of the guild
@@ -70,7 +70,11 @@ class Pro:
 
     @cmd.group(invoke_without_command=True, aliases=["unsync"])
     async def sync(self, ctx):
-        """Sync messages, channel & bans from one to another server"""
+        """
+        Sync messages, channel & bans from one to another server
+
+        The sync command works only in one direction, but you can run the command in both guilds / channel to sync it in both directions.
+        """
         await ctx.invoke(self.bot.get_command("help"), "sync")
 
     @sync.command()
@@ -79,15 +83,125 @@ class Pro:
     @cmd.bot_has_permissions(administrator=True)
     @checks.is_pro()
     async def bans(self, ctx, guild_id: int):
-        pass
+        guild = self.bot.get_guild(guild_id)
+        if guild is None:
+            raise cmd.CommandError(f"There is **no guild with the id** `{guild_id}`.")
 
-    @sync.command(aliases=["messages"])
+        if guild.get_member(ctx.author.id) is None or not guild.get_member(ctx.author.id).guild_permissions.administrator:
+            raise cmd.MissingPermissions([f"administrator` on the guild `{guild.name}"])
+
+        if not guild.me.guild_permissions.administrator:
+            raise cmd.BotMissingPermissions([f"administrator` on the guild `{guild.name}"])
+
+        current = await ctx.db.table("syncs").get(f"{guild.id}{ctx.guild.id}").run(ctx.db.con)
+        types = []
+        if current is not None:
+            types = current.get("types", [])
+
+        if "bans" not in types:
+            types.append("bans")
+            await ctx.send(**ctx.em(f"Successfully **enabled ban sync** from **{guild.name}** to **{ctx.guild.name}**.", type="success"))
+            for reason, user in await guild.bans():
+                try:
+                    await ctx.guild.ban(user, reason=reason)
+                except:
+                    pass
+
+        else:
+            types.remove("bans")
+            await ctx.send(**ctx.em(f"Successfully **disabled ban sync** from **{guild.name}** to **{ctx.guild.name}**.", type="success"))
+
+        await ctx.db.table("syncs").insert({
+            "id": f"{guild.id}{ctx.guild.id}",
+            "types": types,
+            "origin": str(guild.id),
+            "target": str(ctx.guild.id)
+        }, conflict="update").run(ctx.db.con)
+
+    async def on_member_ban(self, guild, user):
+        syncs = await self.bot.db.table("syncs").get_all(str(guild.id), index="origin").filter(lambda s: s["types"].contains("bans")).run(self.bot.db.con)
+        while await syncs.fetch_next():
+            sync = await syncs.next()
+            target = self.bot.get_guild(sync.get("target"))
+            if target is None:
+                continue
+
+            try:
+                await target.ban(user, reason=f"Banned on {sync.get('origin')}")
+
+            except:
+                pass
+
+    async def on_member_unban(self, guild, user):
+        syncs = await self.bot.db.table("syncs").get_all(str(guild.id), index="origin").filter(lambda s: s["types"].contains("bans")).run(self.bot.db.con)
+        while await syncs.fetch_next():
+            sync = await syncs.next()
+            target = self.bot.get_guild(int(sync["target"]))
+            if target is None:
+                continue
+
+            try:
+                await target.unban(user, reason=f"Unbanned on {sync['origin']}")
+
+            except:
+                pass
+
+    @sync.command(aliases=["channel"])
     @cmd.guild_only()
     @cmd.has_permissions(administrator=True)
     @cmd.bot_has_permissions(administrator=True)
     @checks.is_pro()
-    async def channel(self, ctx, channel_id):
-        pass
+    async def messages(self, ctx, channel_id: int):
+        channel = self.bot.get_channel(channel_id)
+        if channel is None:
+            raise cmd.CommandError(f"There is **no channel with the id** `{channel_id}`.")
+
+        if channel.guild.get_member(ctx.author.id) is None or not channel.permissions_for(channel.guild.get_member(ctx.author.id)).administrator:
+            raise cmd.MissingPermissions([f"administrator` in the channel `{channel.name}"])
+
+        if not channel.guild.me.guild_permissions.administrator:
+            raise cmd.BotMissingPermissions([f"administrator` in the channel `{channel.name}"])
+
+        current = await ctx.db.table("syncs").get(f"{channel.id}{ctx.channel.id}").run(ctx.db.con)
+        types = []
+        if current is not None:
+            types = current.get("types", [])
+
+        if "messages" not in types:
+            types.append("messages")
+            await ctx.send(**ctx.em(f"Successfully **enabled message sync** from **<#{channel.id}> to <#{ctx.channel.id}>**.", type="success"))
+
+        else:
+            types.remove("messages")
+            await ctx.send(**ctx.em(f"Successfully **disabled message sync** from **<#{channel.id}> to <#{ctx.channel.id}>**.", type="success"))
+
+        await ctx.db.table("syncs").insert({
+            "id": f"{channel.id}{ctx.channel.id}",
+            "types": types,
+            "origin": str(channel.id),
+            "target": str(ctx.channel.id)
+        }, conflict="update").run(ctx.db.con)
+
+    async def on_message(self, msg):
+        if msg.author.discriminator == "0000":
+            return
+
+        wait_for = []
+        syncs = await self.bot.db.table("syncs").get_all(str(msg.channel.id), index="origin").filter(lambda s: s["types"].contains("messages")).run(self.bot.db.con)
+        while await syncs.fetch_next():
+            sync = await syncs.next()
+            target = self.bot.get_channel(int(sync["target"]))
+            if target is None:
+                continue
+
+            webhooks = await target.webhooks()
+            if len(webhooks) == 0:
+                webhook = await target.create_webhook(name="message sync")
+
+            else:
+                webhook = webhooks[0]
+
+            wait_for.append(await webhook.send(username=msg.author.name, avatar_url=msg.author.avatar_url, content=helpers.clean_content(msg.content), embeds=msg.embeds))
 
 
 def setup(bot):
--- utils/helpers.py
+++ utils/helpers.py
@@ -8,3 +8,9 @@ async def async_cursor_to_list(cursor):
 
 def datetime_to_string(datetime):
     return datetime.strftime("%d. %b %Y - %H:%M")
+
+
+def clean_content(content):
+    content = content.replace("@everyone", "@\u200beveryone")
+    content = content.replace("@here", "@\u200bhere")
+    return content
