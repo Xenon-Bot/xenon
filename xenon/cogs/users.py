4259ffd15e2a177f1fa19af436cf78a096f5a9c5
Merlintor <merlinfuchs2001@gmail.com>
2019-07-22 22:11:57 +0200

    Display blacklist as embed

--- xenon/cogs/users.py
+++ xenon/cogs/users.py
@@ -2,9 +2,8 @@ from discord.ext import commands as cmd
 import discord
 from datetime import datetime
 import pytz
-from prettytable import PrettyTable
 
-from utils import formatter, helpers, checks
+from utils import checks
 
 
 class Users(cmd.Cog):
@@ -13,7 +12,7 @@ class Users(cmd.Cog):
 
         @bot.check
         async def not_blacklisted(ctx):
-            entry = await ctx.db.users.find_one({"id": ctx.author.id, "blacklist.state": True})
+            entry = await ctx.db.users.find_one({"_id": ctx.author.id, "blacklist.state": True})
             if entry is None:
                 return True
 
@@ -23,30 +22,23 @@ class Users(cmd.Cog):
     @cmd.group(aliases=["bl"], hidden=True, invoke_without_command=True)
     @checks.has_role_on_support_guild("Staff")
     async def blacklist(self, ctx):
-        table = PrettyTable()
-        table.field_names = ["User", "Reason", "Admin", "Timestamp"]
-
-        blacklist = ctx.db.users.find({"blacklist.state": True})
+        embed = ctx.em("", type="info", title="Blacklist")["embed"]
+        blacklist = ctx.db.blacklist.find({"blacklist.state": True})
         async for entry in blacklist:
             user = await self.bot.fetch_user(entry["_id"])
-            admin = await self.bot.fetch_user(entry["blacklist"]["admin"])
+            embed.add_field(name=f"{user} ({user.id})", value=f"```{entry['blacklist']['reason']}```")
+            if len(embed.fields) == 10:
+                await ctx.send(embed=embed)
+                embed.clear_fields()
 
-            table.add_row([
-                f"{user} ({user.id})",
-                entry["blacklist"]["reason"],
-                f"{admin} ({entry['blacklist']['admin']})",
-                helpers.datetime_to_string(entry["blacklist"]["timestamp"])
-            ])
-
-        pages = formatter.paginate(str(table))
-        for page in pages:
-            await ctx.send(f"```diff\n{page}```")
+        if len(embed.fields) > 0:
+            await ctx.send(embed=embed)
 
     @blacklist.command()
     @checks.has_role_on_support_guild("Admin")
     async def add(self, ctx, user_id: int, *, reason):
         user = await ctx.bot.fetch_user(user_id)
-        await ctx.db.users.update_one({"id": user.id}, {"$set": {
+        await ctx.db.users.update_one({"_id": user.id}, {"$set": {
             "_id": user.id,
             "blacklist": {
                 "state": True,
