beac4ef46833a8a4e1c93001975fcb94239deb2b
Merlintor <merlinfuchs2001@gmail.com>
2019-10-08 01:59:46 +0200

    Implement options

--- xenon/cogs/builder.py
+++ xenon/cogs/builder.py
@@ -5,6 +5,12 @@ import discord
 from utils import helpers
 
 
+def create_permissions(**kwargs):
+    permissions = discord.Permissions.none()
+    permissions.update(**kwargs)
+    return permissions
+
+
 class BuildMenu:
     def __init__(self, ctx):
         self.ctx = ctx
@@ -15,8 +21,8 @@ class BuildMenu:
                 "name": "roles",
                 "options": [
                     ["delete_old_roles", True],
-                    ["staff_role", False],
-                    ["muted_role", False],
+                    ["staff_roles", True],
+                    ["muted_role", True],
                     ["color_roles", False],
                     ["game_specific_roles", False]
                 ]
@@ -25,10 +31,11 @@ class BuildMenu:
                 "name": "channels",
                 "options": [
                     ["delete_old_channels", True],
-                    ["announcement_channel", False],
-                    ["rules_channel", False],
-                    ["staff_channels", False],
-                    ["game_specific_channels", False]
+                    ["info_channels", True],
+                    ["staff_channels", True],
+                    ["general_channels", True],
+                    ["development_channels", False],
+                    ["gaming_channels", False]
                 ]
             }
         ]
@@ -128,52 +135,217 @@ class Builder(cmd.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @cmd.command(aliases=["builder", "bld", "bd"])
+    @cmd.command(aliases=["builder", "bld", "bd"], hidden=True)
     async def build(self, ctx):
         menu = BuildMenu(ctx)
         options = await menu.run()
 
         roles = {"staff": [], "muted": []}
 
-        if options.get("delete_old_channels"):
+        if options["delete_old_channels"]:
             for channel in ctx.guild.channels:
                 await channel.delete()
 
-        if options.get("delete_old_roles"):
+        if options["delete_old_roles"]:
             for role in filter(lambda r: not r.managed and not r.is_default(), ctx.guild.roles):
                 await role.delete()
 
-        if options.get("staff_roles"):
+        if options["staff_roles"]:
             staff_roles = [
                 {
-                    "name": "Admin"
+                    "name": "Owner",
+                    "color": discord.Color.blurple(),
+                    "permissions": discord.Permissions.all()
                 },
                 {
-                    "name": "Moderator"
+                    "name": "Admin",
+                    "color": discord.Color.dark_red(),
+                    "permissions": discord.Permissions.all()
+                },
+                {
+                    "name": "Moderator",
+                    "color": discord.Color.teal(),
+                    "permissions": create_permissions(
+                        kick_members=True,
+                        ban_members=True,
+                        view_audit_log=True,
+                        priority_speaker=True,
+                        mute_members=True,
+                        deafen_members=True,
+                        move_members=True,
+                        manage_nicknames=True
+                    )
                 }
             ]
 
             for kwargs in staff_roles:
                 roles["staff"].append(await ctx.guild.create_role(**kwargs))
 
-        if options.get("color_roles"):
+        if options["muted_role"]:
+            roles["muted"].append(await ctx.guild.create_role(
+                name="Muted",
+                color=discord.Color.dark_grey(),
+                permissions=create_permissions(
+                    send_messages=False,
+                    add_reactions=False,
+                    connect=False
+                )
+            ))
+
+        if options["color_roles"]:
             color_roles = [
                 {
-                    "name": "Red"
+                    "name": "Green",
+                    "color": discord.Color.green()
                 },
                 {
-                    "name": "Green"
+                    "name": "Blue",
+                    "color": discord.Color.blue()
                 },
                 {
-                    "name": "Yellow"
-                }
+                    "name": "Purple",
+                    "color": discord.Color.purple()
+                },
+                {
+                    "name": "Magenta",
+                    "color": discord.Color.magenta()
+                },
+                {
+                    "name": "Orange",
+                    "color": discord.Color.orange()
+                },
+                {
+                    "name": "Red",
+                    "color": discord.Color.red()
+                },
             ]
 
             for kwargs in color_roles:
                 await ctx.guild.create_role(**kwargs)
 
-        if options.get("muted_role"):
-            roles["muted"].append(await ctx.guild.create_role(name="Muted"))
+        if options["game_specific_roles"]:
+            game_roles = ["minecraft", "fortnite", "pubg", "roblox"]
+            for name in game_roles:
+                await ctx.guild.create_role(name=name)
+
+        if options["info_channels"]:
+            info_category = await ctx.guild.create_category(
+                name="Info",
+                overwrites={
+                    ctx.guild.default_role: discord.PermissionOverwrite(
+                        send_messages=False
+                    ),
+                    **{role: discord.PermissionOverwrite(
+                        send_messages=True
+                    ) for role in roles["staff"]}
+                }
+            )
+
+            channels = ["announcements", "faq", "rules"]
+            for name in channels:
+                await info_category.create_text_channel(name=name)
+
+        if options["staff_channels"]:
+            staff_category = await ctx.guild.create_category(
+                name="Staff",
+                overwrites={
+                    ctx.guild.default_role: discord.PermissionOverwrite(
+                        read_messages=False,
+                        send_messages=False,
+                        connect=False
+                    ),
+                    **{role: discord.PermissionOverwrite(
+                        read_messages=True,
+                        send_messages=True,
+                        connect=True
+                    ) for role in roles["staff"]}
+                }
+            )
+
+            text_channels = ["staff general", "staff commands"]
+            for name in text_channels:
+                await staff_category.create_text_channel(name=name)
+
+            await staff_category.create_voice_channel(name="Staff Voice")
+
+        if options["general_channels"]:
+            general_category = await ctx.guild.create_category(
+                name="General",
+                overwrites={role: discord.PermissionOverwrite(
+                    read_messages=False,
+                    send_messages=False,
+                    connect=False
+                ) for role in roles["muted"]}
+            )
+
+            text_channels = ["general", "shitpost", "commands"]
+            for name in text_channels:
+                await general_category.create_text_channel(name=name)
+
+            await general_category.create_voice_channel(name="General")
+
+        if options["development_channels"]:
+            dev_category = await ctx.guild.create_category(
+                name="Development",
+                overwrites={role: discord.PermissionOverwrite(
+                    read_messages=False,
+                    send_messages=False,
+                    connect=False
+                ) for role in roles["muted"]}
+            )
+
+            text_channels = ["python", "javascript", "java", "kotlin", "c", "go", "ruby"]
+            for name in text_channels:
+                await dev_category.create_text_channel(name=name)
+
+        if options["gaming_channels"]:
+            game_category = await ctx.guild.create_category(
+                name="Gaming",
+                overwrites={role: discord.PermissionOverwrite(
+                    read_messages=False,
+                    send_messages=False,
+                    connect=False
+                ) for role in roles["muted"]}
+            )
+
+            text_channels = ["gaming general", "team finding"]
+            for name in text_channels:
+                await game_category.create_text_channel(name=name)
+
+            voice_channels = [
+                {
+                    "name": "Free 1"
+                },
+                {
+                    "name": "Free 2"
+                },
+                {
+                    "name": "Duo 1",
+                    "user_limit": 2
+                },
+                {
+                    "name": "Duo 2",
+                    "user_limit": 2
+                },
+                {
+                    "name": "Trio 1",
+                    "user_limit": 3
+                },
+                {
+                    "name": "Trio 2",
+                    "user_limit": 3
+                },
+                {
+                    "name": "Squad 1",
+                    "user_limit": 4
+                },
+                {
+                    "name": "Squad 2",
+                    "user_limit": 4
+                }
+            ]
+            for kwargs in voice_channels:
+                await game_category.create_voice_channel(**kwargs)
 
 
 def setup(bot):
