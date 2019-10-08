24fc16680cc85ff773a058e2c8c2431e9a55846e
Merlintor <merlinfuchs2001@gmail.com>
2019-10-08 17:35:17 +0200

    Query values across shards

--- xenon/bot.py
+++ xenon/bot.py
@@ -3,8 +3,11 @@ from discord.ext import commands as cmd
 from motor.motor_asyncio import AsyncIOMotorClient
 import aioredis
 import json
+import uuid
+import asyncio
+import traceback
 
-from utils import formatter, logger
+from utils import formatter, logger, helpers
 from utils.extended import Context
 
 
@@ -76,13 +79,57 @@ class Xenon(cmd.AutoShardedBot):
         shards = await self.get_shards()
         return sum([shard["users"] for shard in shards])
 
-    async def _broadcast_reader(self):
+    async def _shards_reader(self):
         eval_channel, = await self.redis.subscribe("shards")
         async for msg in eval_channel.iter(decoder=json.loads):
-            self.dispatch("broadcast", msg["author"], msg["data"])
+            try:
+                _type, author, data = msg["t"], msg["a"], msg["d"]
+                if _type == "b":
+                    self.dispatch("broadcast", author, data)
+
+                elif _type == "q":
+                    result = eval(data["e"])
+                    await self.redis.publish_json("shards", {
+                        "t": "r",
+                        "a": self.shard_ids,
+                        "d": {"n": data["n"], "r": result}
+                    })
+
+                elif _type == "r":
+                    self.dispatch("shard_response", author, data)
+
+            except Exception:
+                traceback.print_exc()
 
     async def broadcast(self, data):
-        return await self.redis.publish_json("shards", {"author": self.shard_ids, "data": data})
+        return await self.redis.publish_json("shards", {
+            "t": "b",
+            "a": self.shard_ids,
+            "d": data
+        })
+
+    async def query(self, expression, timeout=0.5):
+        nonce = str(uuid.uuid4())
+        await self.redis.publish_json("shards", {
+            "t": "q",
+            "a": self.shard_ids,
+            "d": {"n": nonce, "e": expression}
+        })
+
+        responses = []
+        try:
+            async for author, data in helpers.IterWaitFor(
+                    self,
+                    event="shard_response",
+                    check=lambda a, d: d["n"] == nonce,
+                    timeout=timeout
+            ):
+                responses.append((author, data["r"]))
+
+        except asyncio.TimeoutError:
+            pass
+
+        return responses
 
     @property
     def em(self):
@@ -98,7 +145,7 @@ class Xenon(cmd.AutoShardedBot):
 
     async def start(self, *args, **kwargs):
         self.redis = aioredis.Redis(await aioredis.create_pool("redis://" + self.config.redis_host))
-        self.loop.create_task(self._broadcast_reader())
+        self.loop.create_task(self._shards_reader())
 
         return await super().start(*args, **kwargs)
 
