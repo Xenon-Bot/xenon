3f1b793421ebe10d9c5b436b03640faaa1238256
Merlintor <merlinfuchs2001@gmail.com>
2019-10-08 16:58:35 +0200

    Implement broadcasting across shards

--- xenon/bot.py
+++ xenon/bot.py
@@ -2,6 +2,7 @@ from aiohttp import ClientSession
 from discord.ext import commands as cmd
 from motor.motor_asyncio import AsyncIOMotorClient
 import aioredis
+import json
 
 from utils import formatter, logger
 from utils.extended import Context
@@ -38,15 +39,8 @@ class Xenon(cmd.AutoShardedBot):
 
         self.log.info(f"Loaded {len(self.cogs)} cogs")
 
-    async def on_shard_ready(self, shard_id):
-        self.log.info(f"Shard {shard_id} ready")
-
     async def on_ready(self):
-        self.log.info(
-            f"Fetched {sum([g.member_count for g in self.guilds])} members on {len(self.guilds)} guilds")
-
-    async def on_resumed(self):
-        self.log.debug(f"Bot resumed")
+        self.log.info(f"Fetched {sum([g.member_count for g in self.guilds])} members on {len(self.guilds)} guilds")
 
     async def on_message(self, message):
         if message.author.bot:
@@ -71,7 +65,8 @@ class Xenon(cmd.AutoShardedBot):
         return self.get_guild(self.config.support_guild) is not None
 
     async def get_shards(self):
-        return [{"id": shard.pop("_id"), **shard} for shard in await self.db.shards.find().to_list(self.shard_count or 1)]
+        return [{"id": shard.pop("_id"), **shard} for shard in
+                await self.db.shards.find().to_list(self.shard_count or 1)]
 
     async def get_guild_count(self):
         shards = await self.get_shards()
@@ -80,6 +75,14 @@ class Xenon(cmd.AutoShardedBot):
     async def get_user_count(self):
         shards = await self.get_shards()
         return sum([shard["users"] for shard in shards])
+
+    async def _broadcast_reader(self):
+        eval_channel, = await self.redis.subscribe("shards")
+        async for msg in eval_channel.iter(decoder=json.loads):
+            self.dispatch("broadcast", msg["author"], msg["data"])
+
+    async def broadcast(self, data):
+        return await self.redis.publish_json("shards", {"author": self.shard_ids, "data": data})
 
     @property
     def em(self):
@@ -95,6 +98,8 @@ class Xenon(cmd.AutoShardedBot):
 
     async def start(self, *args, **kwargs):
         self.redis = aioredis.Redis(await aioredis.create_pool("redis://" + self.config.redis_host))
+        self.loop.create_task(self._broadcast_reader())
+
         return await super().start(*args, **kwargs)
 
     def run(self):
--- xenon/cogs/sharding.py
+++ xenon/cogs/sharding.py
@@ -2,64 +2,12 @@ from discord.ext import commands as cmd
 import traceback
 import asyncio
 from datetime import datetime
-import aioredis
-import uuid
-import json
-import inspect
-from contextlib import redirect_stdout
-import io
-import textwrap
-import discord
-
-
-class PublishReturn:
-    def __init__(self, redis: aioredis.Redis):
-        self.redis = redis
-        self.channel = None
-        self.responses = []
-
-    async def _response_reader(self):
-        async for msg in self.channel.iter(decoder=json.loads):
-            self.responses.append(msg)
-
-    async def _wait_for_responses(self, nonce, timeout=0.5):
-        responses = []
-
-        async def with_timeout():
-            while True:
-                for i, response in enumerate(self.responses):
-                    if response["nonce"] == nonce:
-                        responses.append(self.responses.pop(i))
-
-                await asyncio.sleep(0)
-
-        try:
-            await asyncio.wait_for(with_timeout(), timeout=timeout)
-        except asyncio.TimeoutError:
-            pass
-
-        return responses
-
-    async def start(self, loop, channel):
-        self.channel = channel
-        loop.create_task(self._response_reader())
-
-    async def respond(self, msg, json):
-        await self.redis.publish_json(self.channel.name, {"nonce": msg["nonce"], **json})
-
-    async def publish(self, channel, json: dict, timeout=0.5):
-        nonce = str(uuid.uuid4())
-        await self.redis.publish_json(channel, {"nonce": nonce, **json})
-        return await self._wait_for_responses(nonce, timeout=timeout)
 
 
 class Sharding(cmd.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.bot.loop.create_task(self.update_loop())
-        self.bot.loop.create_task(self.subscribe())
-
-        self.pubre = None
 
     async def update_database(self):
         latencies = self.bot.latencies
@@ -80,116 +28,10 @@ class Sharding(cmd.Cog):
         while not self.bot.is_closed():
             try:
                 await self.update_database()
-            except:
+            except Exception:
                 traceback.print_exc()
 
             await asyncio.sleep(60)
-
-    async def _eval_reader(self, channel):
-        async for msg in channel.iter(decoder=json.loads):
-            try:
-                to_eval = msg["statement"].replace("await ", "")
-                try:
-                    result = eval(to_eval)
-                    if inspect.isawaitable(result):
-                        result = await result
-                except Exception as e:
-                    result = type(e).__name__ + ": " + str(e)
-
-                await self.pubre.respond(msg, {"result": result})
-            except Exception:
-                traceback.print_exc()
-
-    async def _exec_reader(self, channel):
-        async for msg in channel.iter(decoder=json.loads):
-            try:
-                env = {
-                    'bot': self.bot,
-                    "self": self,
-                    "config": self.bot.config
-                }
-
-                env.update(globals())
-                stdout = io.StringIO()
-                to_compile = f'async def func():\n{textwrap.indent(msg["body"], "  ")}'
-
-                try:
-                    exec(to_compile, env)
-                except Exception as e:
-                    await self.pubre.respond(msg, {"success": 0, "result": str(e.__class__.__name__)})
-                    return
-
-                func = env['func']
-                try:
-                    with redirect_stdout(stdout):
-                        ret = await func()
-
-                except Exception:
-                    await self.pubre.respond(msg, {"success": 0, "result": str(traceback.format_exc())})
-                else:
-                    value = stdout.getvalue()
-
-                    if ret is None:
-                        await self.pubre.respond(msg, {"success": 1, "result": str(value)})
-                    else:
-                        await self.pubre.respond(msg, {"success": 1, "result": str(value) + str(ret)})
-            except Exception:
-                traceback.print_exc()
-
-    async def subscribe(self):
-        await self.bot.wait_until_ready()
-        eval_channel, exec_channel, response_channel = await self.bot.redis.subscribe("eval", "exec", "responses")
-        self.bot.loop.create_task(self._eval_reader(eval_channel))
-        self.bot.loop.create_task(self._exec_reader(exec_channel))
-        self.pubre = PublishReturn(self.bot.redis)
-        await self.pubre.start(self.bot.loop, response_channel)
-
-    @staticmethod
-    def _format_results(operator, results):
-        operators = {
-            "+": lambda rts: sum([float(r) for r in rts]),
-            "~": lambda rts: sum([float(r) for r in rts]) / len(rts)
-        }
-        operator_func = operators.get(operator, lambda rts: (operator + " ").join([str(r) for r in rts]))
-        return operator_func(results)
-
-    @cmd.command(hidden=True)
-    @cmd.is_owner()
-    async def geval(self, ctx, timeout: float, operator: str, *, statement):
-        statement = statement.strip("`")
-        results = await self.pubre.publish("eval", {"statement": statement}, timeout=timeout)
-        formatted_results = str(self._format_results(operator, [r['result'] for r in results]))
-        if len(formatted_results) > 1800:
-            fp = io.BytesIO(formatted_results.encode())
-            await ctx.send(files=[discord.File(fp, filename="results.txt")])
-
-        else:
-            await ctx.send(
-                f"__{len(results)} Results:__```{formatted_results}```"
-            )
-
-    @cmd.command(hidden=True)
-    @cmd.is_owner()
-    async def gexec(self, ctx, timeout: float, operator: str, *, body):
-        def cleanup_code(content):
-            """Automatically removes code blocks from the code."""
-            # remove ```py\n```
-            if content.startswith('```') and content.endswith('```'):
-                return '\n'.join(content.split('\n')[1:-1])
-
-            # remove `foo`
-            return content.strip('` \n')
-
-        results = await self.pubre.publish("exec", {"body": cleanup_code(body)}, timeout=timeout)
-        formatted_results = str(self._format_results(operator, [r['result'] for r in results]))
-        if len(formatted_results) > 1800:
-            fp = io.BytesIO(formatted_results.encode())
-            await ctx.send(files=[discord.File(fp, filename="results.txt")])
-
-        else:
-            await ctx.send(
-                f"__{len(results)} Results:__```{formatted_results}```"
-            )
 
 
 def setup(bot):
