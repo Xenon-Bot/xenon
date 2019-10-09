b321a333559b06b7a8cc25d0cb5bea9554c6a626
Merlintor <merlinfuchs2001@gmail.com>
2019-10-09 23:22:44 +0200

    minor

--- xenon/utils/logger.py
+++ xenon/utils/logger.py
@@ -9,11 +9,10 @@ def setup():
 
     fh = TimedRotatingFileHandler(f"logs/bot.log", when="midnight", interval=1)
     fh.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(name)s > %(message)s'))
+    logger.addHandler(fh)
 
     ch = logging.StreamHandler(sys.stdout)
     ch.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
-
-    logger.addHandler(fh)
     logger.addHandler(ch)
 
     logging.getLogger("aiohttp.access").parent = None  # Disable access log
