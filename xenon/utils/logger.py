b55b6170913902c434a4734fd3f5216406004530
Merlintor <merlinfuchs2001@gmail.com>
2019-10-09 17:00:33 +0200

    Different log format

--- xenon/utils/logger.py
+++ xenon/utils/logger.py
@@ -8,10 +8,10 @@ def setup():
     logger.setLevel(logging.INFO)
 
     fh = TimedRotatingFileHandler(f"logs/bot.log", when="midnight", interval=1)
-    fh.setFormatter(logging.Formatter('%(asctime)s | [%(levelname)s] %(name)s > %(message)s'))
+    fh.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(name)s > %(message)s'))
 
     ch = logging.StreamHandler(sys.stdout)
-    ch.setFormatter(logging.Formatter('%(asctime)s | [%(levelname)s] %(name)s > %(message)s'))
+    ch.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
 
     logger.addHandler(fh)
     logger.addHandler(ch)
