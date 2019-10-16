22d382f4d92ce8f70f379f8451b8c4550099491e
Merlintor <merlinfuchs2001@gmail.com>
2019-10-16 16:12:56 +0200

    Add more color roles
    Resolves #93

--- xenon/cogs/builder.py
+++ xenon/cogs/builder.py
@@ -236,28 +236,84 @@ class Builder(cmd.Cog):
                     "name": "──── Colors ────"
                 },
                 {
-                    "name": "Green",
-                    "color": discord.Color.green()
+                    "name": "Black",
+                    "color": discord.Color(0)
                 },
                 {
                     "name": "Blue",
-                    "color": discord.Color.blue()
+                    "color": discord.Color(0x4363D8)
                 },
                 {
-                    "name": "Purple",
-                    "color": discord.Color.purple()
+                    "name": "Brown",
+                    "color": discord.Color(0x9A6324)
+                },
+                {
+                    "name": "Cyan",
+                    "color": discord.Color(0x42D4F4)
+                },
+                {
+                    "name": "Green",
+                    "color": discord.Color(0x3CB44B)
+                },
+                {
+                    "name": "Grey",
+                    "color": discord.Color(0xA9A994)
+                },
+                {
+                    "name": "Lavender",
+                    "color": discord.Color(0xE6BEFF)
+                },
+                {
+                    "name": "Lime",
+                    "color": discord.Color(0xBFE743)
                 },
                 {
                     "name": "Magenta",
-                    "color": discord.Color.magenta()
+                    "color": discord.Color(0xF032E6)
+                },
+                {
+                    "name": "Maroon",
+                    "color": discord.Color(0x800014)
+                },
+                {
+                    "name": "Mint",
+                    "color": discord.Color(0xAAFFC3)
+                },
+                {
+                    "name": "Navy",
+                    "color": discord.Color(0x000075)
+                },
+                {
+                    "name": "Olive",
+                    "color": discord.Color(0x808012)
                 },
                 {
                     "name": "Orange",
-                    "color": discord.Color.orange()
+                    "color": discord.Color(0xF58231)
+                },
+                {
+                    "name": "Pink",
+                    "color": discord.Color(0xF4BCBE)
+                },
+                {
+                    "name": "Purple",
+                    "color": discord.Color(0x911EB4)
                 },
                 {
                     "name": "Red",
-                    "color": discord.Color.red()
+                    "color": discord.Color(0xE62345)
+                },
+                {
+                    "name": "Teal",
+                    "color": discord.Color(0x469990)
+                },
+                {
+                    "name": "White",
+                    "color": discord.Color(0xFFFFFF)
+                },
+                {
+                    "name": "Yellow",
+                    "color": discord.Color(0xFFE119)
                 },
             ]
 
