11d5b52777e4fcf30e497d867e95f279c5065053
Merlintor <merlinfuchs2001@gmail.com>
2019-12-23 19:00:04 +0100

    Add healthcheck script

---
+++ xenon/healthcheck.py
@@ -0,0 +1,21 @@
+from aiohttp import ClientSession
+from asyncio import get_event_loop
+
+# Using aiohttp because it's install anyways
+
+
+async def check():
+    session = ClientSession()
+    resp = await session.request("GET", "http://localhost:9090/health")
+    await session.close()
+
+    if resp.status == 200:
+        exit(0)
+
+    else:
+        exit(1)
+
+
+if __name__ == "__main__":
+    loop = get_event_loop()
+    loop.run_until_complete(check())
